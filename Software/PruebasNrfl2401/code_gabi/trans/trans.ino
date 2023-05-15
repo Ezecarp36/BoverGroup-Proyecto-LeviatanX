ec512ad2f05908e321a177d3b87c0eb280c4da0a
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-05-14 23:03:15 -0300

    test gabi

---
+++ Software/PruebasNrfl2401/code_gabi/trans/trans.ino
@@ -0,0 +1,26 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
+#define CE 27
+#define CSN 15
+RF24 radio (CE, CSN);
+const uint64_t canal = 0xE8E8F0F0E1LL;
+
+int dato1 = 1;
+int dato2 = 2;
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(canal); // Con laa función "radio.openWritingPipe(canal)" se abre el canal de comunicación con el receptor.
+  radio.setPALevel(RF24_PA_MIN); // Con la función "radio.setPALevel(RF24_PA_MIN)" y su parámetro "RF24_PA_MIN" se configura el módulo en baja potencia.
+  radio.stopListening(); // Con la función "radio.stopListening()" se define al módulo como transmisor.
+}
+
+void loop() {
+  radio.write(&dato1,sizeof(dato1)); // La función "radio.write()" envía la información.
+  radio.write(&dato2,sizeof(dato2));
+  delay(200);
+}
