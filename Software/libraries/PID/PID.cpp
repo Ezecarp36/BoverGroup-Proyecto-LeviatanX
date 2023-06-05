e2550252ad0cabb9121b9fd1050b2dba9e696612
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-06-04 21:00:37 -0300

    comento libreria

--- Software/libraries/PID/PID.cpp
+++ Software/libraries/PID/PID.cpp
@@ -13,11 +13,12 @@ Pid::Pid(double p, double i, double d,double sp, double tick)
 
 double Pid::ComputePid(double inp)
 {
-    if(millis() > currentTimePID + tick_pid)
+    if(millis() > currentTimePID + tick_pid)//realiza un millis cada cierto tiempo y actualiza la lectura pid . debe ser lo mas minimo 
     {
         currentTimePID = millis();
-        input = inp;
-        error = input - setPoint;
+        input = inp; 
+        error = input - setPoint; //calulo error
+        //prevenir problemas de "windup"
         if (integral < integralMin){
             integral = integralMin;
         }
@@ -25,14 +26,14 @@ double Pid::ComputePid(double inp)
             integral = integralMax;
         }
         else 
-            integral += error;
+            integral += error; //suma los errores pasados
 
-        derivada = error - lastError;
-        double out = kp * error+ ti * integral + td * derivada;
-        lastError = error;
+        derivada = error - lastError; //toma importancia en los cambios bruscos por eso error - error pasado 
+        double out = kp * error+ ti * integral + td * derivada; //calculo salida pid
+        lastError = error; // actualiza error pasado
 
-        return out;
+        return out; //retorna pid  
     }
-    else return 0.0;
+    else return 0.0;// si ocurre algo con millis devuelve un 0.0 como error
     
 }
